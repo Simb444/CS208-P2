1ce7982e2373b5617c7e791ed42bbf260c1fa30f
Simb444 <Arjun.bott@gmail.com>
2022-10-01 02:18:47 -0400

    MVP

--- src/Recursive.java
+++ src/Recursive.java
@@ -6,9 +6,9 @@
 import java.util.Scanner;
 
 public class Recursive {
-	/* attributes
-	* @author: David Smirnov
-	*/
+	/** attributes
+	 * @author: David Smirnov
+	 */
 	private static IllegalArgumentException NOT_ODD = new IllegalArgumentException("You may only enter odd");
 	private int[] binaryNumber;
 
@@ -21,15 +21,19 @@ public class Recursive {
 	}
 
 	/** custom constructor
-	  * @author: David Smirnov
-	  */
+	 * @author: David Smirnov
+	 */
 	public Recursive(String input) {
-		Scanner scan = new Scanner(input);
+		Scanner scan = new Scanner(prepare_input(input));
 
 		{ // local scope
 			int i;
-			for (i = 0; scan.hasNextInt(); i++) {
-				scan.nextInt();
+			for (i = 0; scan.hasNext(); i++) {
+				try {
+					Integer.parseInt(scan.next());
+				} catch (NumberFormatException e) {
+					i--;
+				}
 			}
 
 			if (i % 2 == 0) {
@@ -39,33 +43,52 @@ public class Recursive {
 			binaryNumber = new int[i];
 		}
 
-		scan = new Scanner(input);
-		for (int i = 0; scan.hasNextInt(); i++) {
-			binaryNumber[i] = scan.nextInt();
+		scan = new Scanner(prepare_input(input));
+		for (int i = 0; scan.hasNext(); i++) {
+			binaryNumber[i] = Integer.parseInt(scan.next());
 		}
+	}
+
+	/** helper method
+	 * @author: Arjun Bott
+	 * method used to seperate bits in the input
+	 */
+	private static String prepare_input(String input) {
+		String output = "";
+
+		for (int i = 0; i < input.length(); i++) {
+			output += input.charAt(i) + " ";
+		}
+
+		return output.trim();
 	}
 
 	/**
 	 * overload for recursive method
 	 * @author: David Smirnov
-	 * receives the original binary number stored as our attribute,
 	 *  calls itself by passing the original attribute and sets index to 0 by default
 	 */
-	private static int[] onion(int[] input){
-		return onion(input, 0);
+	public int[] onion() {
+		return onion(0);
 	}
 
 	/**
 	 * overloaded Recursive method
 	 * @author: Arjun Bott
-	 * receives both int[] and int as index value
+	 * receives int as index value
 	 */
-	private static int[] onion(int[] input, int index){
-		if (input.length % 2 == 0) {
+	public int[] onion(int index) {
+		if (binaryNumber.length % 2 == 0) { // check binaryNumber for odd or even value
 			throw NOT_ODD;
 		} else {
-			input[index] = input[index] == 0 ? 1 : 0;
-			input[input.length - 1 - index] = input[input.length - 1 - index] == 0 ? 1 : 0;
+			if (2 * index - 1 >= binaryNumber.length) { // base case
+				binaryNumber[(int) ((index + 1) / 2)] = binaryNumber[(int) ((index + 1) / 2)] == 0 ? 1 : 0;
+				return binaryNumber;
+			} else { // recursive case
+				binaryNumber[index] = binaryNumber[index] == 0 ? 1 : 0;
+				binaryNumber[binaryNumber.length - 1 - index] = binaryNumber[binaryNumber.length - 1 - index] == 0 ? 1 : 0;
+				return onion(index + 1);
+			}
 		}
 	}
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Tester class
+ * @author: Arjun bott
+ * @author: David Smirnov
+ */
 public class Tester {
 	public static void main(String[] args) {
-		
+		// David
+		Scanner scan = new Scanner(System.in);
+		int numTrials;
+		System.out.println("How many binary numbers do you want to flip?");
+		numTrials = scan.nextInt();
+		scan.nextLine();
+		System.out.println();
+
+		// Arjun
+			System.out.println("numTrials: " + numTrials);
+		// for (int i = 0; i < numTrials; i++) {
+			System.out.println("Enter a binary number: ");
+			// System.out.println();
+			Recursive re = new Recursive(scan.nextLine());
+			int[] binaryNumber = re.onion();
+			// System.out.println();
+			System.out.println("binaryNumber length: " + binaryNumber.length);
+			for (int j = 0; j < binaryNumber.length; j++) {
+				System.out.print(binaryNumber[j]);
+			}
+			System.out.println();
+
+
+		// }
 	}
 }
